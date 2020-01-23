4b313d2e70e924809f897b1432e8f09c0056281a
Nicolas Haddad <nhaddad@eso.org>
2020-01-23 16:49:54 -0300

    Added int.py

---
+++ pydtk/inst/inst.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 22 21:35:41 2020
+
+@author: nhaddad
+"""
+
+MUSE = {'Qdrant':[(0,1000,0,1000),(0,1000,0,1000),(0,1000,0,1000),(0,1000,0,1000)],
+
+       'ext':['CHAN01',
+              'CHAN02',
+              'CHAN03',
+              'CHAN04',
+              'CHAN05',
+              'CHAN06',
+              'CHAN07',
+              'CHAN08',
+              'CHAN09',
+              'CHAN10',
+              'CHAN11',
+              'CHAN12',
+              'CHAN13',
+              'CHAN14',
+              'CHAN15',
+              'CHAN16',
+              'CHAN17',
+              'CHAN18',
+              'CHAN19',
+              'CHAN20',
+              'CHAN21',
+              'CHAN22',
+              'CHAN23',
+              'CHAN24'
+              ]}
+
+OMEGACAM = {'shape':(0,1000,0,1000),
+            'ext':['CHAN01',
+              'CHAN02',
+              'CHAN03',
+              'CHAN04',
+              'CHAN05',
+              'CHAN06',
+              'CHAN07',
+              'CHAN08',
+              'CHAN09',
+              'CHAN10',
+              'CHAN11',
+              'CHAN12',
+              'CHAN13',
+              'CHAN14',
+              'CHAN15',
+              'CHAN16',
+              'CHAN17',
+              'CHAN18',
+              'CHAN19',
+              'CHAN20',
+              'CHAN21',
+              'CHAN22',
+              'CHAN23',
+              'CHAN24'
+              ]}
+
+VIRCAM = {'shape':(0,1000,0,1000),
+            'ext':['DET1.CHIP1',
+              'DET1.CHIP2',
+              'DET1.CHIP3',
+              'DET1.CHIP4',
+              'DET1.CHIP5',
+              'DET1.CHIP6',
+              'DET1.CHIP7',
+              'DET1.CHIP8',
+              'DET1.CHIP9',
+              'DET1.CHIP10',
+              'DET1.CHIP11',
+              'DET1.CHIP12',
+              'DET1.CHIP13',
+              'DET1.CHIP14',
+              'DET1.CHIP15',
+              'DET1.CHIP16'
+              ]}
+HAWKI = {'shape':(2048,2048),
+         'ext':[]}
+
+ESPRESSO = {'shape':(9000,9000),
+            'ext':[],
+            'Qdrant':[(), (), ()]}
